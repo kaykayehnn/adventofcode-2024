0ba659fbe19fc925cf998024cbf1816da2d50d67
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2025-01-02 15:02:08 +0200

    Add optimized solution

--- day-22/Part2.cs
+++ day-22/Part2.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using System.Diagnostics;
 
 class Part2
@@ -17,72 +16,49 @@ class Part2
     {
         return a % 16777216;
     }
-
-    public static long SolveTask(int c1)
+    public static void Solve(string[] args)
     {
         var input = File.ReadAllLines("input.txt");
+        var s = Stopwatch.StartNew();
         var numbers = input.Select(long.Parse).ToArray();
-        var totalBest = 0L;
-        var sequences = new long[numbers.Length][];
+        var sequences = new int[numbers.Length][];
         for (int i = 0; i < numbers.Length; i++)
         {
-            sequences[i] = new long[2001];
+            // We need to track the first 2000 changes, which means 2001 elements.
+            sequences[i] = new int[2001];
             var secretNumber = numbers[i];
-            sequences[i][0] = secretNumber % 10;
+            sequences[i][0] = (int)(secretNumber % 10);
             for (int j = 1; j < sequences[i].Length; j++)
             {
                 secretNumber = Prune(Mix(secretNumber, secretNumber * 64));
                 secretNumber = Prune(Mix(secretNumber, secretNumber / 32));
                 secretNumber = Prune(Mix(secretNumber, secretNumber * 2048));
-                sequences[i][j] = secretNumber % 10;
+                sequences[i][j] = (int)(secretNumber % 10);
             }
         }
-        for (int c2 = -9; c2 < 10; c2++)
+
+        var changeSequences = new int[20, 20, 20, 20];
+        var best = 0;
+        for (int i = 0; i < numbers.Length; i++)
         {
-            if (c1 + c2 < -9 || c1 + c2 > 9) continue;
-            for (int c3 = -9; c3 < 10; c3++)
+            var foundSequences = new bool[20, 20, 20, 20];
+            var arr = sequences[i];
+            var secretNumber = numbers[i];
+            for (int j = 1; j < arr.Length - 3; j++)
             {
-                if (c1 + c2 + c3 < -9 || c1 + c2 + c3 > 9) continue;
+                // Offset each of the diffs by 10 to account for negative numbers.
+                var c1 = arr[j] - arr[j - 1] + 10;
+                var c2 = arr[j + 1] - arr[j] + 10;
+                var c3 = arr[j + 2] - arr[j + 1] + 10;
+                var c4 = arr[j + 3] - arr[j + 2] + 10;
+                if (foundSequences[c1, c2, c3, c4]) continue;
 
-                for (int c4 = -9; c4 < 10; c4++)
-                {
-                    if (c1 + c2 + c3 + c4 < -9 || c1 + c2 + c3 + c4 > 9) continue;
-
-                    var subTotal = 0L;
-                    for (int i = 0; i < numbers.Length; i++)
-                    {
-                        var arr = sequences[i];
-                        for (int j = 1; j < arr.Length - 3; j++)
-                        {
-                            if (arr[j] - arr[j - 1] == c1 && arr[j + 1] - arr[j] == c2 && arr[j + 2] - arr[j + 1] == c3 && arr[j + 3] - arr[j + 2] == c4)
-                            {
-                                subTotal += arr[j+3];
-                                break;
-                            }
-                        }
-                    }
-                    if (totalBest < subTotal) totalBest = subTotal;
-                }
+                foundSequences[c1, c2, c3, c4] = true;
+                changeSequences[c1, c2, c3, c4] += arr[j + 3];
+                if (changeSequences[c1, c2, c3, c4] > best) best = changeSequences[c1, c2, c3, c4];
             }
         }
-
-        return totalBest;
-    }
-
-    public static void Solve(string[] args)
-    {
-        var s = Stopwatch.StartNew();
-        var tasks = new List<Task<long>>();
-        for (int c1 = -9; c1 < 10; c1++)
-        {
-            int c1Copy = c1;
-            tasks.Add(Task.Run(() => SolveTask(c1Copy)));
-        }
-
-        Task.WaitAll(tasks.ToArray());
-        var totalBest = tasks.Max(t => t.Result);
-
-        System.Console.WriteLine(totalBest);
+        System.Console.WriteLine(best);
         System.Console.WriteLine(s.ElapsedMilliseconds);
     }
 }
---
+++ day-22/Part2BruteForce.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+class Part2BruteForce
+{
+    public static long Mix(long a, long b)
+    {
+        return a ^ b;
+    }
+
+    public static long Prune(long a)
+    {
+        return a % 16777216;
+    }
+
+    public static long SolveTask(int c1)
+    {
+        var input = File.ReadAllLines("input.txt");
+        var numbers = input.Select(long.Parse).ToArray();
+        var totalBest = 0L;
+        var sequences = new long[numbers.Length][];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sequences[i] = new long[2001];
+            var secretNumber = numbers[i];
+            sequences[i][0] = secretNumber % 10;
+            for (int j = 1; j < sequences[i].Length; j++)
+            {
+                secretNumber = Prune(Mix(secretNumber, secretNumber * 64));
+                secretNumber = Prune(Mix(secretNumber, secretNumber / 32));
+                secretNumber = Prune(Mix(secretNumber, secretNumber * 2048));
+                sequences[i][j] = secretNumber % 10;
+            }
+        }
+        for (int c2 = -9; c2 < 10; c2++)
+        {
+            if (c1 + c2 < -9 || c1 + c2 > 9) continue;
+            for (int c3 = -9; c3 < 10; c3++)
+            {
+                if (c1 + c2 + c3 < -9 || c1 + c2 + c3 > 9) continue;
+
+                for (int c4 = -9; c4 < 10; c4++)
+                {
+                    if (c1 + c2 + c3 + c4 < -9 || c1 + c2 + c3 + c4 > 9) continue;
+
+                    var subTotal = 0L;
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        var arr = sequences[i];
+                        for (int j = 1; j < arr.Length - 3; j++)
+                        {
+                            if (arr[j] - arr[j - 1] == c1 && arr[j + 1] - arr[j] == c2 && arr[j + 2] - arr[j + 1] == c3 && arr[j + 3] - arr[j + 2] == c4)
+                            {
+                                subTotal += arr[j+3];
+                                break;
+                            }
+                        }
+                    }
+                    if (totalBest < subTotal) totalBest = subTotal;
+                }
+            }
+        }
+
+        return totalBest;
+    }
+
+    public static void Solve(string[] args)
+    {
+        var s = Stopwatch.StartNew();
+        var tasks = new List<Task<long>>();
+        for (int c1 = -9; c1 < 10; c1++)
+        {
+            int c1Copy = c1;
+            tasks.Add(Task.Run(() => SolveTask(c1Copy)));
+        }
+
+        Task.WaitAll(tasks.ToArray());
+        var totalBest = tasks.Max(t => t.Result);
+
+        System.Console.WriteLine(totalBest);
+        System.Console.WriteLine(s.ElapsedMilliseconds);
+    }
+}
